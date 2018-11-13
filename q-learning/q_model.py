bdde5010902c33bed4812c79ddb61380ff52da65
ruotingw <wruoting@gmail.com>
2018-11-12 23:17:36 -0500

    major updates to the state structure

--- q-learning/q_model.py
+++ q-learning/q_model.py
@@ -2,33 +2,19 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
-from keras.layers.recurrent import LSTM
+from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.optimizers import RMSprop, Adam
 
 
 def q_model():
-    tsteps = 1
-    batch_size = 1
-    num_features = 2
-
     model = Sequential()
-    model.add(LSTM(64,
-                   input_shape=(1, num_features),
-                   return_sequences=True,
-                   stateful=False))
-    model.add(Dropout(0.5))
-
-    model.add(LSTM(64,
-                   input_shape=(1, num_features),
-                   return_sequences=False,
-                   stateful=False))
-    model.add(Dropout(0.5))
-
-    model.add(Dense(3, init='lecun_uniform'))
-    model.add(Activation('linear'))  # linear output so we can have range of real-valued outputs
+    model.add(Dense(1))
+    model.add(Activation('relu'))
+    model.add(Dense(3))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(3, activation='linear'))
 
     adam = Adam()
-    model.compile(loss='mse', optimizer=adam)
+    model.compile(loss='mse', optimizer=adam, metrics = ['mse'])
 
     return model
