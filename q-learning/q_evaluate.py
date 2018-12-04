e33bcfd0c8bddfe7e57ef8e76def1330748689b7
ruotingw <wruoting@gmail.com>
2018-12-03 21:54:47 -0500

    changes in naming etc.

--- q-learning/q_evaluate.py
+++ q-learning/q_evaluate.py
@@ -10,14 +10,14 @@ import random
 
 def q_evaluate():
     # reset time step to evaluate the total reward
-    df_bullish = csv_to_dataframe('sin-real.csv')
-    plot_name = 'Sine-Real.html'
-    model = load_model('q_model_sine.h5')
+    df_bullish = csv_to_dataframe('btc-test.csv')
+    plot_name = 'BTC-Test.html'
+    model = load_model('q_model_btc_train.h5')
     stored_buffer = 50
     batch_range = range(0, 15)
     state, price_data, unscaled_price_data = processing_data(df_bullish, batch_range)
     decision_state = pd.Series(index=np.arange(len(price_data)))
-    eval_reward, decision_state = evaluate_q_epoch(state, price_data, model, decision_state, batch_range, stored_buffer)
+    eval_reward, decision_state = evaluate_q_epoch(state, price_data, unscaled_price_data, model, decision_state, batch_range, stored_buffer)
     print("Reward:" + str(eval_reward))
     # Init all states and actions
     decision_state_dataframe = pd.DataFrame(np.transpose([df_bullish['Time'],decision_state]))
@@ -27,7 +27,7 @@ def q_evaluate():
     graph([df_bullish], [buy_to_map], [sell_to_map], plot_name)
 
 
-def evaluate_q_epoch(state, price_data, model, decision_state, batch_range, stored_buffer):
+def evaluate_q_epoch(state, price_data, unscaled_price_data, model, decision_state, batch_range, stored_buffer):
     eval_reward = 0
     terminal_state = False
     state_index = 0
@@ -39,7 +39,7 @@ def evaluate_q_epoch(state, price_data, model, decision_state, batch_range, stor
         q_value = q_value[0][0]
         action = np.argmax(q_value)
         state_index, decision_state, terminal_state = take_action(action, decision_state, state, state_index, stored_buffer, batch_range, eval=True)
-        action_to_reward = get_reward(state_index, action, price_data, decision_state, batch_size)
+        action_to_reward = get_reward(state_index, action, price_data, unscaled_price_data, decision_state, batch_size, eval=True)
         eval_reward += action_to_reward
         if state_index < max_length - 1:
             state_index += 1
